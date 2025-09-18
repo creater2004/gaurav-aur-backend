e1a0dbbd2e7a4b70a68cf31cdfa44cdb45ddd92f
creater2004 <gaurav1ktiwari@gmail.com>
2025-09-18 12:25:33 +0530

    change in index file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ dotenv.config({ path: "./env" });
 import connectDB from "./db/index.js";
 
 connectDB()
-  .then(process.env, PORT || 5000, () => {
+  .then(process.env.PORT || 5000, () => {
     console.log(`Server is running on port ${process.env.PORT || 5000}`);
   })
   .catch((error) => {
